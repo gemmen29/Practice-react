622966c3fdb5f6c7d75f8216d2c29e63ec0a39b5
George Magdy <georgtriple@gmail.com>
2021-12-29 11:08:56 +0200

    Add first-app intialization

---
+++ first-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="display-flex">
+      <h1>Hiiiiiiiiiiiii</h1>
+      <h2>byeeeeeeeeeeee</h2>
+    </div>
+  );
+}
+
+export default App;
