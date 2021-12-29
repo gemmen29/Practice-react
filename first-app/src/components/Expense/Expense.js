6dc8c6ce0c2a44ae5fb17becbbd2a9c71e5e9abb
George Magdy <georgtriple@gmail.com>
2021-12-29 11:16:42 +0200

    Add Expense Component

---
+++ first-app/src/components/Expense/Expense.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "./Expense.css";
+import Card from "../Card/Card";
+import ExpenseItem from "../ExpenseItem/ExpenseItem";
+
+const Expense = (props) => (
+  <Card className="Expense">
+    <ExpenseItem
+      title={props.items[0].title}
+      amount={props.items[0].amount}
+      date={props.items[0].date}
+    />
+    <ExpenseItem
+      title={props.items[1].title}
+      amount={props.items[1].amount}
+      date={props.items[1].date}
+    />
+    <ExpenseItem
+      title={props.items[2].title}
+      amount={props.items[2].amount}
+      date={props.items[2].date}
+    />
+    <ExpenseItem
+      title={props.items[3].title}
+      amount={props.items[3].amount}
+      date={props.items[3].date}
+    />
+  </Card>
+);
+
+export default Expense;
