e6e12db131f4fb1e4e2c4d2d4056ff48104ef1ee
George Magdy <georgtriple@gmail.com>
2021-12-30 22:23:16 +0200

    Fix amount issue

--- first-app/src/components/NewExpense/ExpenseForm/ExpenseForm.js
+++ first-app/src/components/NewExpense/ExpenseForm/ExpenseForm.js
@@ -23,7 +23,7 @@ const ExpenseForm = (props) => {
 
     const expenseData = {
       title: enteredTitle,
-      amount: enteredAmount,
+      amount: parseInt(enteredAmount),
       date: new Date(enteredDate),
     };
 
