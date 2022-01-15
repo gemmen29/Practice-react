ffab798c315fe8036c91f4f0e79a782e04cc7650
George Magdy <georgtriple@gmail.com>
2022-01-15 20:55:19 +0200

    Display counter from store in counter component

--- practice-redux/src/components/Counter.js
+++ practice-redux/src/components/Counter.js
@@ -1,12 +1,14 @@
-import classes from './Counter.module.css';
+import classes from "./Counter.module.css";
+import { useSelector } from "react-redux";
 
 const Counter = () => {
   const toggleCounterHandler = () => {};
+  const counter = useSelector((state) => state.counter);
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{counter}</div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
