331991316ed053ece92e1ce2cd532e797ab0d114
George Magdy <georgtriple@gmail.com>
2022-01-15 20:51:29 +0200

    Use Provider in index and small refactor

--- practice-redux/src/reducers/counterReducer.js
+++ practice-redux/src/reducers/counterReducer.js
@@ -1,16 +1,19 @@
-export default (state={counter = 0}, action) => {
-  switch(action.type) {
+const counterReducerDefaultState = { counter: 0 };
+
+const counterReducer = (state = counterReducerDefaultState, action) => {
+  switch (action.type) {
     case "INCREMENT":
       return {
         ...state,
-        counter: state.counter + 1
-      }
+        counter: state.counter + 1,
+      };
     case "DECREMENT":
       return {
         ...state,
-        counter: state.counter - 1
-      }
+        counter: state.counter - 1,
+      };
     default:
       return state;
   }
-}
+};
+export default counterReducer;
