c492cd37403fa0d4abee3162c417b3d58633d889
George Magdy <georgtriple@gmail.com>
2022-01-16 03:24:27 +0200

    Add auth reducer to store

--- practice-redux/src/store/index.js
+++ practice-redux/src/store/index.js
@@ -1,8 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterSlice from "../slices/counterSlice";
+import authSlice from "../slices/authSlice";
 
 const store = configureStore({
-  reducer: counterSlice.reducer,
+  reducer: { counter: counterSlice.reducer, auth: authSlice.reducer },
 });
 
 export default store;
