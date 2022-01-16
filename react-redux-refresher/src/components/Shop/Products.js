ef6ecc68c4afa28a4683fde431f3e8e00ac9a758
George Magdy <georgtriple@gmail.com>
2022-01-16 09:26:25 +0200

    Render and refactor products

--- react-redux-refresher/src/components/Shop/Products.js
+++ react-redux-refresher/src/components/Shop/Products.js
@@ -1,16 +1,22 @@
-import ProductItem from './ProductItem';
-import classes from './Products.module.css';
+import ProductItem from "./ProductItem";
+import classes from "./Products.module.css";
+import { useSelector } from "react-redux";
 
 const Products = (props) => {
+  const products = useSelector((state) => state.products.products);
+
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+        {products.map((product) => (
+          <ProductItem
+            key={product.id}
+            title={product.name}
+            price={product.price}
+            description={product.description}
+          />
+        ))}
       </ul>
     </section>
   );
--- react-redux-refresher/src/slices/productsSlice.js
+++ react-redux-refresher/src/slices/productsSlice.js
@@ -3,8 +3,20 @@ import { v4 as uuidv4 } from "uuid";
 
 const initialState = {
   products: [
-    { id: uuidv4(), price: 10, quantity: 1, name: "Product 1" },
-    { id: uuidv4(), price: 10, quantity: 1, name: "Product 2" },
+    {
+      id: uuidv4(),
+      price: 10,
+      quantity: 1,
+      name: "Product 1",
+      description: "This is product 1 description",
+    },
+    {
+      id: uuidv4(),
+      price: 15,
+      quantity: 1,
+      name: "Product 2",
+      description: "This is product 2 description",
+    },
   ],
 };
 const productsSlice = createSlice({
