5309bf87ac9b901d47c1db338add0aea7f0dc208
George Magdy <georgtriple@gmail.com>
2022-01-16 04:19:54 +0200

    Finish todo without redux

--- todo-react/src/App.js
+++ todo-react/src/App.js
@@ -1,24 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/Header/Header";
+import AddNewTodo from "./components/Todo/AddNewTodo/AddNewTodo";
+import DisplayTodo from "./components/Todo/DisplayTodo/DisplayTodo";
+
+const todoKey = "todos";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    if (localStorage.getItem(todoKey)) {
+      setTodos(JSON.parse(localStorage.getItem(todoKey)));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem(todoKey, JSON.stringify(todos));
+  }, [todos]);
+
+  const AddNewTodoHandler = (todo) => {
+    setTodos((preTodos) => [...preTodos, todo]);
+  };
+
+  const ChangeCheckHandler = (todoId) => {
+    setTodos((preTodos) => {
+      const allTodos = [...preTodos];
+      const todo = allTodos.find((todo) => todo.id === todoId);
+      todo.completed = !todo.completed;
+      return [...allTodos];
+    });
+  };
+
+  const deleteHandler = (todoId) => {
+    setTodos((preTodos) => preTodos.filter((todo) => todo.id !== todoId));
+  };
+
+  const editHandler = (todoId, newTitle) => {
+    setTodos((preTodos) => {
+      const allTodos = [...preTodos];
+      const todo = allTodos.find((todo) => todo.id === todoId);
+      todo.title = newTitle;
+      return [...allTodos];
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <div className="d-flex flex-column container justify-content-center align-items-center flex-fill">
+        <AddNewTodo onSubmit={AddNewTodoHandler} />
+        <div className="p-2 w-100 d-flex flex-column gap-2 pt-5">
+          {todos.map((todo) => (
+            <DisplayTodo
+              key={todo.id}
+              todo={todo}
+              onChangeCheck={ChangeCheckHandler}
+              onEdit={editHandler}
+              onDelete={deleteHandler}
+            />
+          ))}
+        </div>
+      </div>
+    </>
   );
 }
 
