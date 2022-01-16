9132d2c9ba2a82294eed3e1ef4fe3eebcdb0a2af
George Magdy <georgtriple@gmail.com>
2022-01-16 03:53:29 +0200

    Finish Simple Authentication

--- practice-redux/src/App.js
+++ practice-redux/src/App.js
@@ -1,12 +1,16 @@
 import Counter from "./components/Counter";
 import Header from "./components/Header";
 import Auth from "./components/Auth";
+import UserProfile from "./components/UserProfile";
+import { useSelector } from "react-redux";
 
 const App = () => {
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
   return (
     <>
       <Header />
-      <Auth />
+      {!isAuthenticated && <Auth />}
+      {isAuthenticated && <UserProfile />}
       <Counter />
     </>
   );
