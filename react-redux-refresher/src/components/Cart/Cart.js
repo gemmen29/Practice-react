262350af938b218cd98f88d2a0b4ac8667cc81af
George Magdy <georgtriple@gmail.com>
2022-01-16 09:37:32 +0200

    Display items in cart

--- react-redux-refresher/src/components/Cart/Cart.js
+++ react-redux-refresher/src/components/Cart/Cart.js
@@ -1,15 +1,27 @@
-import Card from '../UI/Card';
-import classes from './Cart.module.css';
-import CartItem from './CartItem';
+import Card from "../UI/Card";
+import classes from "./Cart.module.css";
+import CartItem from "./CartItem";
+import { useSelector } from "react-redux";
 
-const Cart = (props) => {
+const Cart = () => {
+  const items = useSelector((state) => state.cart.items);
   return (
     <Card className={classes.cart}>
       <h2>Your Shopping Cart</h2>
       <ul>
-        <CartItem
+        {/* <CartItem
           item={{ title: 'Test Item', quantity: 3, total: 18, price: 6 }}
-        />
+        /> */}
+        {items.map((item) => (
+          <CartItem
+            item={{
+              title: item.name,
+              quantity: item.quantity,
+              total: item.price * item.quantity,
+              price: item.price,
+            }}
+          />
+        ))}
       </ul>
     </Card>
   );
