4c71519deb2b6b1d1c0630ecae958ab00b80e319
George Magdy <georgtriple@gmail.com>
2022-01-16 09:59:02 +0200

    Display cart quantity

--- react-redux-refresher/src/components/Cart/CartButton.js
+++ react-redux-refresher/src/components/Cart/CartButton.js
@@ -1,10 +1,12 @@
-import classes from './CartButton.module.css';
+import classes from "./CartButton.module.css";
+import { useSelector } from "react-redux";
 
 const CartButton = (props) => {
+  const totalQuantity = useSelector((state) => state.cart.totalQuantity);
   return (
     <button className={classes.button}>
       <span>My Cart</span>
-      <span className={classes.badge}>1</span>
+      <span className={classes.badge}>{totalQuantity}</span>
     </button>
   );
 };
