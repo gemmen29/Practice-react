9260b9a480412a18dddffadee2f97cfa4d122ab6
George Magdy <georgtriple@gmail.com>
2022-01-16 09:19:08 +0200

    Add product slice to store

--- react-redux-refresher/src/store/index.js
+++ react-redux-refresher/src/store/index.js
@@ -1,9 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartSlice from "../slices/cartSlice";
 import cartVisibleSlice from "../slices/cartVisibleSlice";
+import productsSlice from "../slices/productsSlice";
 
 const store = configureStore({
-  reducer: { cartVisible: cartVisibleSlice.reducer, cart: cartSlice.reducer },
+  reducer: {
+    cartVisible: cartVisibleSlice.reducer,
+    cart: cartSlice.reducer,
+    products: productsSlice.reducer,
+  },
 });
 
 export default store;
