be1238c2650b74867c5ccfe60a43af9a3148dff1
George Magdy <georgtriple@gmail.com>
2022-05-28 10:06:33 +0200

    Prepare meetup project

--- first-nextjs-app/pages/news/[newsId].js
+++
@@ -1,9 +0,0 @@
-import styles from '../../styles/Home.module.css';
-import { useRouter } from 'next/router';
-
-function DetailPage() {
-  const router = useRouter();
-  return <h1 className={styles.news}>Detail Page {router.query.newsId}</h1>;
-}
-
-export default DetailPage;
